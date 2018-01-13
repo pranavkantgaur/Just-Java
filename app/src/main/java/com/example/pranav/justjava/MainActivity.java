82019ae93294b3c4bce79b46af776a4b0e76771a
pranavkantgaur <pranav8iisc@gmail.com>
2018-01-13 14:12:39 +0530

    Adds options for addons

--- app/src/main/java/com/example/pranav/justjava/MainActivity.java
+++ app/src/main/java/com/example/pranav/justjava/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.TextureView;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
@@ -19,20 +20,40 @@ public class MainActivity extends AppCompatActivity {
     }
     static int quantity = 0;
     final int pricePerUnit = 5;
-
+    static boolean isWhippedCreamChecked = false;
+    static boolean isChocolateChecked = false;
     // this method is called when 'ORDER' button is pressed.
     public void submitOrder(View view)
     {
-        //display(quantity);
-        displayPrice(quantity * pricePerUnit);
-        //String message = "Free";
-        //displayMessage(message);
+        orderSummary();
     }
+
+    private void orderSummary()
+    {
+        String message = "Name: Mr. Pranav";
+        message += "\nAdd whipped cream?";
+        if (isWhippedCreamChecked)
+            message += "true";
+        else
+            message += "false";
+        message += "\nAdd chocolate?";
+        if (isChocolateChecked)
+            message += "true";
+        else
+            message += "false";
+        message += "\nQuantity: " + quantity;
+        message += "\nTotal: #" + quantity * pricePerUnit;
+        message += "\nThank you!";
+        TextView view = (TextView) findViewById(R.id.order_summary_textview);
+        view.setText(message);
+
+    }
+
+
     public void increaseQuantity(View view)
     {
         quantity++;
-        display(quantity);
-        //displayPrice(quantity * pricePerUnit);
+        displayQuantity(quantity);
     }
 
     public void decreaseQuantity(View view)
@@ -41,25 +62,29 @@ public class MainActivity extends AppCompatActivity {
             quantity--;
         else
             quantity = 0;
-        display(quantity);
-       // displayPrice(quantity * pricePerUnit);
+        displayQuantity(quantity);
     }
-    private void displayPrice(int number)
-    {
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        String message = "Total: " + NumberFormat.getCurrencyInstance().format(number).toString();
-        message += "\nThank you!!";
-        priceTextView.setText(message);
-    }
-    private void display(int number)
+
+    private void displayQuantity(int number)
     {
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
         quantityTextView.setText("" + number);
     }
-    private void displayMessage(String message)
-    {
-        TextView    priceTextView = (TextView) findViewById(R.id.price_text_view);
-        priceTextView.setText(message);
+
+    public void setIsWhippedCreamChecked(View view) {
+        CheckBox whippedCreamCB = (CheckBox) findViewById(R.id.whipped_cream_checkbox);
+        if (whippedCreamCB.isChecked())
+            isWhippedCreamChecked = true;
+        else
+            isWhippedCreamChecked = false;
     }
 
+    public void setIsChocolateChecked(View view)
+    {
+        CheckBox chocolateCB = (CheckBox) findViewById(R.id.chocolate_checkbox);
+        if (chocolateCB.isChecked())
+            isChocolateChecked = true;
+        else
+            isChocolateChecked = false;
+    }
 }
