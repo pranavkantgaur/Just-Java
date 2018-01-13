20424a7a2059a0137f1401c840a055eb18df94cb
pranavkantgaur <pranav8iisc@gmail.com>
2018-01-13 20:04:30 +0530

    Invokes Email intent.

--- app/src/main/java/com/example/pranav/justjava/MainActivity.java
+++ app/src/main/java/com/example/pranav/justjava/MainActivity.java
@@ -1,11 +1,15 @@
 package com.example.pranav.justjava;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.TextureView;
 import android.view.View;
 import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
@@ -19,26 +23,71 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
     static int quantity = 0;
-    final int pricePerUnit = 5;
     static boolean isWhippedCreamChecked = false;
     static boolean isChocolateChecked = false;
     // this method is called when 'ORDER' button is pressed.
     public void submitOrder(View view)
     {
-        orderSummary();
-    }
 
-    private void orderSummary()
-    {
-        String message = "Name: Mr. Pranav";
+//        orderSummary();
+        String senderAddress  = "justjava@example.com";
+
+        String message;
+        int pricePerUnit = 5;
+        EditText custName = (EditText) findViewById(R.id.customer_name_text);
+        String subject = "Just Java Order for " + custName.getText().toString();
+        message = "Name: " + custName.getText().toString();
         message += "\nAdd whipped cream?";
         if (isWhippedCreamChecked)
+        {
             message += "true";
+            pricePerUnit += 1;
+        }
         else
             message += "false";
         message += "\nAdd chocolate?";
         if (isChocolateChecked)
+        {
             message += "true";
+            pricePerUnit += 2;
+        }
+        else
+            message += "false";
+        message += "\nQuantity: " + quantity;
+        message += "\nTotal: #" + quantity * pricePerUnit;
+        message += "\nThank you!";
+
+        // setup intents
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:"));
+        intent.putExtra(Intent.EXTRA_EMAIL, senderAddress);
+        intent.putExtra(Intent.EXTRA_SUBJECT, subject);
+        intent.putExtra(Intent.EXTRA_TEXT, message);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+
+    }
+    private void orderSummary()
+    {
+
+        int pricePerUnit = 5;
+        EditText custName = (EditText) findViewById(R.id.customer_name_text);
+        String message = "Name: " + custName.getText().toString();
+        message += "\nAdd whipped cream?";
+        if (isWhippedCreamChecked)
+        {
+            message += "true";
+            pricePerUnit += 1;
+        }
+        else
+            message += "false";
+        message += "\nAdd chocolate?";
+        if (isChocolateChecked)
+        {
+            message += "true";
+            pricePerUnit += 2;
+        }
         else
             message += "false";
         message += "\nQuantity: " + quantity;
@@ -56,12 +105,14 @@ public class MainActivity extends AppCompatActivity {
         displayQuantity(quantity);
     }
 
-    public void decreaseQuantity(View view)
-    {
+    public void decreaseQuantity(View view) {
         if (quantity > 1)
             quantity--;
         else
-            quantity = 0;
+        {
+            Toast.makeText(this, "You cannot have less than 1 coffee", Toast.LENGTH_SHORT).show();
+            return;
+        }
         displayQuantity(quantity);
     }
 
